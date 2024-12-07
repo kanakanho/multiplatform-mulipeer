d72848901588b1e543f7f48dc43f39091155bebb
kanakanho <kahoshibakichi@gmail.com>
2024-12-07 22:23:22 +0900

    Initial Commit

---
+++ multiplatform-mulipeer/multiplatform_mulipeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  multiplatform_mulipeerApp.swift
+//  multiplatform-mulipeer
+//
+//  Created by blueken on 2024/12/07.
+//
+
+import SwiftUI
+
+@main
+struct multiplatform_mulipeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ multiplatform-mulipeerTests/multiplatform_mulipeerTests.swift
@@ -0,0 +1,16 @@
+//
+//  multiplatform_mulipeerTests.swift
+//  multiplatform-mulipeerTests
+//
+//  Created by blueken on 2024/12/07.
+//
+
+import Testing
+
+struct multiplatform_mulipeerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
